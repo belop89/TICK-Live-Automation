64efe4fc13e28e79e362606c4c9778209e2b527e
belop <gamliroth@gmail.com>
2026-05-09 23:29:54 +0200

    Updated build script to automatically use version number from CMake

--- build_win.bat
+++ build_win.bat
@@ -1,4 +1,7 @@
 @echo off
+
+for /f "tokens=3 delims=) " %%a in ('findstr /c:"project(TICK VERSION" CMakeLists.txt') do set VERSION=%%a
+
 echo Configuring CMake for VST3...
 cmake -B ./build -DCOPY_AFTER_BUILD="FALSE" -DCMAKE_CXX_FLAGS="/DJUCE_DISPLAY_SPLASH_SCREEN=0"
 
@@ -8,5 +11,5 @@ cmake --build ./build --clean-first --target TICK_VST3 --config RelWithDebInfo
 echo Building Windows Installer (NSIS)...
 cmake --build ./build --target PACKAGE --config RelWithDebInfo
 
-echo Build and Packaging Complete! Look in the /build/ folder for TICK-Live-Automation-[version].exe
+echo Build and Packaging Complete! Look in the /build/ folder for TICK-Live-Automation-%VERSION%.exe
 pause
